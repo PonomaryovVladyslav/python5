ce43490fbd84358ebb3a474846aa7133b002246b
Vlad Ponomarov <vladyslav.ponomarov@workday.com>
2024-03-17 16:49:11 +0100

    add Ama

--- mysite/settings.py
+++ mysite/settings.py
@@ -32,7 +32,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myapp.apps.MyappConfig'
+    'myapp.apps.MyappConfig',
+    'storages'
 ]
 
 MIDDLEWARE = [
--- mysite/settings_prod.py
+++ mysite/settings_prod.py
@@ -14,3 +14,19 @@ DATABASES = {
         'PORT': os.environ.get('DBPORT', '5432'),
     }
 }
+
+# Optional
+AWS_S3_OBJECT_PARAMETERS = {
+    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+    'CacheControl': 'max-age=94608000',
+}
+# Required
+AWS_STORAGE_BUCKET_NAME = 'python5'
+AWS_S3_REGION_NAME = 'us-east-1'  # e.g. us-east-2
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_PUB')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET')
+# НЕ ВПИСЫВАЙТЕ САМИ КЛЮЧИ, ТОЛЬКО os.environ.get('SOME_KEY')
+
+# Tell the staticfiles app to use S3Boto3 storage when writing the collected static files (when
+# you run `collectstatic`).
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
